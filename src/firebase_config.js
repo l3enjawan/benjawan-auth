153d4b942bce5d7103d85a8eb04d744a7a5e31c5
l3enjawan <6022040064.benjawan@gmail.com>
2023-03-28 01:10:21 +0700

    backup

---
+++ src/firebase_config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATebrHzXZTIg22CCdrw0aNpTtWbOx8JZA",
+  authDomain: "benjawan-4e880.firebaseapp.com",
+  projectId: "benjawan-4e880",
+  storageBucket: "benjawan-4e880.appspot.com",
+  messagingSenderId: "376358245553",
+  appId: "1:376358245553:web:6fd60646f216f23b21fe90",
+};
+
+const app = initializeApp(firebaseConfig);
